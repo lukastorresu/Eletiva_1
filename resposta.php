f1e0f2405c124dd6882bc1f4a05b2ea08f86c9be
lukastorresu <lukastorresu@gmail.com>
2025-02-20 21:37:56 -0300

    atualizacao

--- resposta.php
+++ resposta.php
@@ -7,22 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Boa noite, hoje é
-    <?php
-
+    <h1>Resposta1</h1>
+    <?php 
+        if ($_SERVER['REQUEST_METHOD'])
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $soma = $valor1 + $valor2;
+        echo "O valor da soma é: $soma";
+        $div = $valor1 / $valor2;
+        $mult = $valor1 * $valor2;
+        $quad1 = $valor1 * $valor1;
+        $quad2 = $valor2 * $valor2;
         
-        echo date("d/m/Y.");
-    ?>  
-    </h1>
-    <p >
-
-        <?php 
-            $nome = "Lucas";
-            echo "O nome é: " .$nome;
-            
+    
+    
     ?>
-    </p>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
