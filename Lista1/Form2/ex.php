8ab2fbccb114619fe7999dab997675250b43090d
lukastorresu <lukastorresu@gmail.com>
2025-02-27 20:11:11 -0300

    grid

--- Lista1/Form2/ex.php
+++ Lista1/Form2/ex.php
@@ -13,68 +13,76 @@
     
     <form method="post">
                         
-                        <div class="mb-3">
+    <div class="row">
+                        <div class="col">
                             <label for="32" class="form-label">Código</label>
-                            <input type="number" id="32" name="32" class="form-control" disabled="">
+                            <input value="32" placeholder"32" type="number" id="32" name="32" class="form-control" disabled="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="nome" class="form-label">Nome</label>
                             <input type="text" id="nome" name="nome" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="email" class="form-label">E-mail</label>
                             <input type="email" id="email" name="email" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
+                                <label for="cpf" class="form-label">CPF</label>
+                                <input type="number" id="cpf" name="cpf" class="form-control" required="">
+                            </div>
+</div>
+<div class="row">
+                        <div class="col">
                             <label for="celular" class="form-label">Nº Celular</label>
                             <input type="number" id="celular" name="celular" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="telefone" class="form-label">Nº Telefone Fixo </label>
                             <input type="number" id="telefone" name="telefone" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="cep" class="form-label">CEP</label>
                             <input type="number" id="cep" name="cep" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="endereco" class="form-label">Logradouro</label>
                             <input type="text" id="endereco" name="endereco" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="numeroend" class="form-label">Nº</label>
                             <input type="number" id="numeroend" name="numeroend" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="bairro" class="form-label">Bairro</label>
                             <input type="text" id="bairro" name="bairro" class="form-control" required="">
                         </div>
-                    
-                        <div class="mb-3">
+</div>
+<div class="row">
+                        <div class="col">
                             <label for="cidade" class="form-label">Cidade</label>
                             <input type="text" id="cidade" name="cidade" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="uf" class="form-label">UF</label>
                             <input type="text" id="uf" name="uf" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="status" class="form-label">Status</label>
                             <select id="status" name="status" class="form-select" required="">
                                 <option value="opcao1">opcao1</option><option value="opcao2">opcao2</option>
                             </select>
                         </div>
-                    
+</div>
                     <button type="submit" class="btn btn-primary">Enviar</button>
                 </form>
             
