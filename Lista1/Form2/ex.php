ff9e880ae212f72260557375ca3d554118cfc326
lukastorresu <lukastorresu@gmail.com>
2025-02-27 20:07:43 -0300

    formulario inicial bootstrap

---
+++ Lista1/Form2/ex.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Formulário 2</h1>
+    <p>
+        
+    
+    <form method="post">
+                        
+                        <div class="mb-3">
+                            <label for="32" class="form-label">Código</label>
+                            <input type="number" id="32" name="32" class="form-control" disabled="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="nome" class="form-label">Nome</label>
+                            <input type="text" id="nome" name="nome" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input type="email" id="email" name="email" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="celular" class="form-label">Nº Celular</label>
+                            <input type="number" id="celular" name="celular" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="telefone" class="form-label">Nº Telefone Fixo </label>
+                            <input type="number" id="telefone" name="telefone" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="cep" class="form-label">CEP</label>
+                            <input type="number" id="cep" name="cep" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="endereco" class="form-label">Logradouro</label>
+                            <input type="text" id="endereco" name="endereco" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="numeroend" class="form-label">Nº</label>
+                            <input type="number" id="numeroend" name="numeroend" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="bairro" class="form-label">Bairro</label>
+                            <input type="text" id="bairro" name="bairro" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="cidade" class="form-label">Cidade</label>
+                            <input type="text" id="cidade" name="cidade" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="uf" class="form-label">UF</label>
+                            <input type="text" id="uf" name="uf" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="status" class="form-label">Status</label>
+                            <select id="status" name="status" class="form-select" required="">
+                                <option value="opcao1">opcao1</option><option value="opcao2">opcao2</option>
+                            </select>
+                        </div>
+                    
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            
+            
+            
+    </p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
