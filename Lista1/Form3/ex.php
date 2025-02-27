231b52eb0d754dc18842e0685032015a2b6214c6
lukastorresu <lukastorresu@gmail.com>
2025-02-27 20:32:14 -0300

    grid

--- Lista1/Form3/ex.php
+++ Lista1/Form3/ex.php
@@ -35,11 +35,12 @@
                             <input type="number" id="celular" name="celular" class="form-control" required="">
                         </div>
 </div>
+<div class="row">
                         <div class="col">
                             <label for="endereco" class="form-label">Partner Adress</label>
                             <input type="text" id="endereco" name="endereco" class="form-control" required="">
                         </div>
-                    
+                        <div class="row">
                         <div class="col">
                             <label for="datainicial" class="form-label">Contract Start Date</label>
                             <input type="date" id="datainicial" name="datainicial" class="form-control" required="">
@@ -49,7 +50,8 @@
                             <label for="datafinal" class="form-label">Contract Expiry Date</label>
                             <input type="date" id="datafinal" name="datafinal" class="form-control" required="">
                         </div>
-                    
+</div>
+<div class="row">
                         <div class="col">
                             <label for="minloan" class="form-label">Minimum Loan Amount</label>
                             <input type="number" id="minloan" name="minloan" class="form-control" required="">
@@ -59,7 +61,8 @@
                             <label for="maxloan" class="form-label">Maximum Loan Amount</label>
                             <input type="number" id="maxloan" name="maxloan" class="form-control" required="">
                         </div>
-                    
+</div>
+<div class="row">
                         <div class="col">
                             <label for="juros" class="form-label">Interest Rate</label>
                             <input type="number" id="juros" name="juros" class="form-control" required="">
@@ -69,8 +72,10 @@
                             <label for="deposito" class="form-label">Deposit Amount</label>
                             <input type="number" id="deposito" name="deposito" class="form-control" required="">
                         </div>
-                    
+</div>
+
                     <button type="submit" class="btn btn-primary">Save</button>
+
                 </form>
             
             
