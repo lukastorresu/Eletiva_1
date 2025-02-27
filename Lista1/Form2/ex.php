6c6787535ee8dc3d430b72a8942de592e58b004a
lukastorresu <lukastorresu@gmail.com>
2025-02-27 20:18:09 -0300

    formatacao

--- Lista1/Form2/ex.php
+++ Lista1/Form2/ex.php
@@ -14,76 +14,77 @@
     <form method="post">
                         
     <div class="row">
-                        <div class="col">
+                        <div class="col-1">
                             <label for="32" class="form-label">Código</label>
-                            <input value="32" placeholder"32" type="number" id="32" name="32" class="form-control" disabled="">
+                            <input value="32" placeholder="32" type="number" id="32" name="32" class="form-control" disabled="">
                         </div>
                     
                         <div class="col">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="text" id="nome" name="nome" class="form-control" required="">
+                            <input placeholder="Nome Completo do Cliente" type="text" id="nome" name="nome" class="form-control" required="">
                         </div>
                     
                         <div class="col">
                             <label for="email" class="form-label">E-mail</label>
-                            <input type="email" id="email" name="email" class="form-control" required="">
+                            <input placeholder="cliente@dominio.com" type="email" id="email" name="email" class="form-control" required="">
                         </div>
                     
-                        <div class="col">
+                        <div class="col-2 ">
                                 <label for="cpf" class="form-label">CPF</label>
-                                <input type="number" id="cpf" name="cpf" class="form-control" required="">
+                                <input placeholder="Só números" type="number" id="cpf" name="cpf" class="form-control" required="">
                             </div>
 </div>
 <div class="row">
                         <div class="col">
                             <label for="celular" class="form-label">Nº Celular</label>
-                            <input type="number" id="celular" name="celular" class="form-control" required="">
+                            <input placeholder="Nº do celular" type="number" id="celular" name="celular" class="form-control" required="">
                         </div>
                     
                         <div class="col">
                             <label for="telefone" class="form-label">Nº Telefone Fixo </label>
-                            <input type="number" id="telefone" name="telefone" class="form-control" required="">
+                            <input placeholder="Nº telefone" type="number" id="telefone" name="telefone" class="form-control" required="">
                         </div>
                     
                         <div class="col">
                             <label for="cep" class="form-label">CEP</label>
-                            <input type="number" id="cep" name="cep" class="form-control" required="">
+                            <input placeholder="ex:88308070" type="number" id="cep" name="cep" class="form-control" required="">
                         </div>
                     
                         <div class="col">
                             <label for="endereco" class="form-label">Logradouro</label>
-                            <input type="text" id="endereco" name="endereco" class="form-control" required="">
+                            <input placeholder="ex:Eua 1400," type="text" id="endereco" name="endereco" class="form-control" required="">
                         </div>
                     
-                        <div class="col">
+                        <div class="col-1">
                             <label for="numeroend" class="form-label">Nº</label>
-                            <input type="number" id="numeroend" name="numeroend" class="form-control" required="">
+                            <input placeholder="Nº" type="number" id="numeroend" name="numeroend" class="form-control" required="">
                         </div>
                     
                         <div class="col">
                             <label for="bairro" class="form-label">Bairro</label>
-                            <input type="text" id="bairro" name="bairro" class="form-control" required="">
+                            <input placeholder="Bairro" type="text" id="bairro" name="bairro" class="form-control" required="">
                         </div>
 </div>
 <div class="row">
-                        <div class="col">
+                        <div class="col-3">
                             <label for="cidade" class="form-label">Cidade</label>
-                            <input type="text" id="cidade" name="cidade" class="form-control" required="">
+                            <input placeholder="Cidade" type="text" id="cidade" name="cidade" class="form-control" required="">
                         </div>
                     
-                        <div class="col">
+                        <div class="col-1">
                             <label for="uf" class="form-label">UF</label>
-                            <input type="text" id="uf" name="uf" class="form-control" required="">
+                            <input placeholder="UF" type="text" id="uf" name="uf" class="form-control" required="">
                         </div>
                     
-                        <div class="col">
+                        <div class="col-2">
                             <label for="status" class="form-label">Status</label>
-                            <select id="status" name="status" class="form-select" required="">
+                            <select placeholder="Selecione" id="status" name="status" class="form-select" required="">
                                 <option value="opcao1">opcao1</option><option value="opcao2">opcao2</option>
                             </select>
                         </div>
 </div>
-                    <button type="submit" class="btn btn-primary">Enviar</button>
+                      <button type="submit" class="btn btn-danger">Resetar</button>
+                      <button type="submit" class="btn btn-success">Próximo</button>
                 </form>
             
             
