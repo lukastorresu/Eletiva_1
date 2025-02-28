a38e7d6a87f66f3246317459f00f45a59c360664
Lucas Torres <lukastorresu@gmail.com>
2025-02-27 23:10:15 -0300

    grid e botões

--- Lista1/Form4/ex.php
+++ Lista1/Form4/ex.php
@@ -14,53 +14,54 @@
     
     
     <form method="post">
-                        
-                        <div class="mb-3">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" id="nome" name="nome" class="form-control" required="">
+                        <div class="row">
+                        <div class="col">
+                            <label for="nome" class="form-label">Nome:</label>
+                            <input placeholder="Informe o nome..." type="text" id="nome" name="nome" class="form-control" required="">
                         </div>
-                    
-                        <div class="mb-3">
+</div><div class="row">
+                        <div class="col-2">
                             <label for="cpf" class="form-label">CPF:</label>
-                            <input type="number" id="cpf" name="cpf" class="form-control" required="">
+                            <input placeholder="Informe o cpf..." type="number" id="cpf" name="cpf" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
+                        <div class="col">
                             <label for="endereco" class="form-label">Endereço:</label>
-                            <input type="text" id="endereco" name="endereco" class="form-control" required="">
+                            <input placeholder="Informe o endereço..." type="text" id="endereco" name="endereco" class="form-control" required="">
                         </div>
-                    
-                        <div class="mb-3">
+
+                        <div class="col-2">
                             <label for="nivel" class="form-label">Nível:</label>
-                            <select id="nivel" name="nivel" class="form-select" required="">
+                            <select placeholder="---" id="nivel" name="nivel" class="form-select" required="">
                                 <option value="Junior">Junior</option><option value="Pleno">Pleno</option><option value="Senior">Senior</option>
                             </select>
                         </div>
-                    
-                        <div class="mb-3">
+                        </div><div class="row">
+                        <div class="col">
                             <label for="email" class="form-label">Email:</label>
-                            <input type="email" id="email" name="email" class="form-control" required="">
+                            <input placeholder="Informe o email..." type="email" id="email" name="email" class="form-control" required="">
                         </div>
+                
                     
-                        <div class="mb-3">
+                        <div class="col-3">
                             <label for="senha" class="form-label">Senha:</label>
-                            <input type="hidden" id="senha" name="senha" class="form-control" required="">
+                            <input placeholder="Informe a senha..." type="password" id="senha" name="senha" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
-                            <label for="senha" class="form-label">Senha:</label>
-                            <input type="text" id="senha" name="senha" class="form-control" required="">
-                        </div>
-                    
-                        <div class="mb-3">
+                        <div class="col-2">
                             <label for="status" class="form-label">Status:</label>
-                            <select id="status" name="status" class="form-select" required="">
+                            <select placeholder="---" id="status" name="status" class="form-select" required="">
                                 <option value="Ativo">Ativo</option><option value="Inativo">Inativo</option>
                             </select>
                         </div>
+                        <div class="row">
+                        <div class="col-9">
+</div>
+                            <div class="col">
+                    <button type="submit" class="btn btn-success">Enviar</button>
+                    <button type="submit" class="btn btn-light">Cancelar</button>
+</div>
                     
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                    <button type="submit" class="btn btn-light">Cancelars</button>
                 </form>
             
             
