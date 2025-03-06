8be643610e52d8d20f35642c8e67874f931b5a94
Lucas Torres <lukastorresu@gmail.com>
2025-03-05 22:32:33 -0300

    Título, Labels, e nova variavel

--- Lista2/Exer5/index.php
+++ Lista2/Exer5/index.php
@@ -7,20 +7,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercício 4 - Dividir Valores</h1>
+    <h1>Exercício 5 - Calcular Média</h1>
     <p>
         
     <form method="post" action="resposta.php">
                         
     <div class="row">
                         <div class="col-2">
-                            <label for="valor1" class="form-label">Valor 1</label>
+                            <label for="valor1" class="form-label">Nota 1</label>
                             <input type="number" id="valor1" name="valor1" class="form-control" required="">
                         </div>
                     
                         <div class="col-2">
-                            <label for="valor2" class="form-label">Valor 2</label>
+                            <label for="valor2" class="form-label">Nota 2</label>
                             <input type="number" id="valor2" name="valor2" class="form-control" required="">
+                        </div>
+
+                        <div class="col-2">
+                            <label for="valor3" class="form-label">Nota 3</label>
+                            <input type="number" id="valor3" name="valor3" class="form-control" required="">
                         </div>
 </div>
                     
