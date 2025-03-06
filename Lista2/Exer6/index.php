18b9f0d3e56e24075ad848bea6f337ab117ee0de
Lucas Torres <lukastorresu@gmail.com>
2025-03-05 22:42:48 -0300

    Título e Grid corrigidos

--- Lista2/Exer6/index.php
+++ Lista2/Exer6/index.php
@@ -7,26 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercício 5 - Calcular Média</h1>
+    <h1>Exercício 6 - Converter Graus Celsius em Fahrenheit</h1>
     <p>
         
     <form method="post" action="resposta.php">
                         
     <div class="row">
-                        <div class="col-2">
-                            <label for="valor1" class="form-label">Nota 1</label>
+                        <div class="col-3">
+                            <label for="valor1" class="form-label">Temperatura em Graus Celsius:</label>
                             <input type="number" id="valor1" name="valor1" class="form-control" required="">
                         </div>
                     
-                        <div class="col-2">
-                            <label for="valor2" class="form-label">Nota 2</label>
-                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
-                        </div>
-
-                        <div class="col-2">
-                            <label for="valor3" class="form-label">Nota 3</label>
-                            <input type="number" id="valor3" name="valor3" class="form-control" required="">
-                        </div>
 </div>
                     
                     <button type="submit" class="btn btn-primary">Enviar</button>
