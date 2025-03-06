8f4ed6d0025499b3098b6ba1f45ee96316f2eb16
Lucas Torres <lukastorresu@gmail.com>
2025-03-06 10:53:41 -0300

    Formatação/Identação
    Códigos formatados para melhor leitura

--- Lista2/Exer3/index.php
+++ Lista2/Exer3/index.php
@@ -1,33 +1,36 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Exercício 3 - Multiplicar Valores</h1>
-    <p>
-        
-    <form method="post" action="resposta.php">
-                        
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Exercício 3 - Multiplicar Valores</h1>
+  <p>
+
+  <form method="post" action="resposta.php">
+
     <div class="row">
-                        <div class="col-2">
-                            <label for="valor1" class="form-label">Valor 1</label>
-                            <input type="number" id="valor1" name="valor1" class="form-control" required="">
-                        </div>
-                    
-                        <div class="col-2">
-                            <label for="valor2" class="form-label">Valor 2</label>
-                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
-                        </div>
-</div>
-                    
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </form>
-            
-    </p>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+      <div class="col-2">
+        <label for="valor1" class="form-label">Valor 1</label>
+        <input type="number" id="valor1" name="valor1" class="form-control" required="">
+      </div>
+
+      <div class="col-2">
+        <label for="valor2" class="form-label">Valor 2</label>
+        <input type="number" id="valor2" name="valor2" class="form-control" required="">
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </form>
+
+  </p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
