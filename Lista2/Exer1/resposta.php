88b0457c46877c369ea18ce5ecdab4fa01116615
Lucas Torres <lukastorresu@gmail.com>
2025-03-05 22:09:07 -0300

    Renomeei Títulos

--- Lista2/Exer1/resposta.php
+++ Lista2/Exer1/resposta.php
@@ -8,6 +8,7 @@
   </head>
   <body>
     <h1>Resposta</h1>
+    <p>
     <?php 
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             try {
@@ -19,11 +20,8 @@
                     echo $e->getMessage();
                 }
         }
-     
-        
     
-    
-    ?>
+    ?></p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
