0f73eb4f5b83cf2c89efd3108e6f053473c349ec
Lucas Torres <lukastorresu@gmail.com>
2025-03-05 22:02:28 -0300

    Grid e variáveis corrigidos

--- Lista2/Exer1/index.php
+++ Lista2/Exer1/index.php
@@ -7,21 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-  <h1>Exemplo
+  <h1>Somar valores
     <p> 
     
     <form method="post" action="resposta.php">
-                        
-                        <div class="mb-3">
-                            <label for="telefone" class="form-label">Telefone Residencial</label>
-                            <input type="number" id="telefone" name="valor1" class="form-control" required="">
+                        <div class="row">
+                        <div class="col-2">
+                            <label for="valor1" class="form-label">Valor 1</label>
+                            <input type="number" id="valor1" name="valor1" class="form-control" required="">
                         </div>
                     
-                        <div class="mb-3">
-                            <label for="celular" class="form-label">Celular</label>
-                            <input type="number" id="celular" name="valor2" class="form-control" required="">
+                        <div class="col-2">
+                            <label for="valor2" class="form-label">Valor 2</label>
+                            <input type="number" id="valor2" name="valor2" class="form-control" required="">
                         </div>
-                    
+</div>
                     <button type="submit" class="btn btn-primary">Enviar</button>
                 </form>
             
