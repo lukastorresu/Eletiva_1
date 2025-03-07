afda24eb08f02b9d49e333d036398574db96cf87
Lucas Torres <lukastorresu@gmail.com>
2025-03-07 09:03:39 -0300

    Formulário Inicial
    copiado do exercício 16

---
+++ Lista2/Exer17/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+<h1>Exercício 16 - Calcular Desconto</h1>
+  <p>
+
+  <form method="post" action="resposta.php">
+
+  <div class="row">
+      <div class="col-2">
+        <label for="preco" class="form-label">Preço:</label>
+        <input type="text" id="preco" name="preco" class="form-control" required="">
+      </div>
+
+      <div class="col-2">
+        <label for="desconto" class="form-label">Porcentagem do Desconto:</label>
+        <input type="text" id="desconto" name="desconto" class="form-control" required="">
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </form>
+
+  </p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
