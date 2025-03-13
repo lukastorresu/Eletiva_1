89698ce471804e67bdfffe21d32e4d8625da3080
lukastorresu <lukastorresu@gmail.com>
2025-03-13 19:41:03 -0300

    Formulário Inicial
    Copiado do exercício 4

---
+++ Lista3/Exer5/resposta.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Resposta</h1>
+  <p>
+    <?php
+    $valor1 = $_POST['valor1'];
+    $valor1 = floatval($_POST['valor1']);
+    if ($valor1 > 100.00){
+      $novo_valor = $valor1 - ($valor1 * 0.15);
+      echo "O novo valor do produto com desconto de 15% é igual a: R$ $novo_valor";
+    }else{
+      echo "O valor do produto é igual ou inferior a R$100,00 .";
+    }
+    
+    ?></p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
