eac1aee1157d6bccdb050ebe853a885e511808bb
lukastorresu <lukastorresu@gmail.com>
2025-03-13 20:09:37 -0300

    Exibição melhorada

--- Lista3/Exer7/resposta.php
+++ Lista3/Exer7/resposta.php
@@ -19,8 +19,8 @@
       $cont = $cont + $i;
       $i++;
 }
-    echo $cont;
-    
+    echo "A soma dos valores de 1 a $valor1 é: $cont";
+
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
