887a7e4b34f8f3ddb228c1caa4f373a000c8a272
lukastorresu <lukastorresu@gmail.com>
2025-03-13 20:15:43 -0300

    Corrigido While + quebra de linha

--- Lista3/Exer8/resposta.php
+++ Lista3/Exer8/resposta.php
@@ -15,9 +15,9 @@
     $valor1 = $_POST['valor1'];
     $i = 1;
     do {
-      echo $valor1;
+      echo $valor1."<br>";
       $valor1--;
-    } while ($valor1 > 1);
+    } while ($valor1+1 > 1);
 
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
