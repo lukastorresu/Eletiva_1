875416e7d933222c3a9447097568e56b201275e3
Lucas Torres <lukastorresu@gmail.com>
2025-03-13 08:22:40 -0300

    verificação corrigida
    verificado qual valor é menor, apresentado em ordem crescente, e apresentado apenas 1 vez se iguais.

--- Lista3/Exer3/resposta.php
+++ Lista3/Exer3/resposta.php
@@ -15,18 +15,19 @@
     $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
     $menor_valor = $valor1;
-    if ($valor2 < $menor_valor){   
+    $maior_valor = $valor2;
+    if ($valor1 == $valor2){
+      echo "O valores são iguais: $valor1";
+    }elseif ($valor2 < $menor_valor){   
       $menor_valor = $valor2;
       $maior_valor = $valor1;
-    }else if($valor1 = $valor2){   
-      $menor_valor = ;
-
+      echo "Os valores em ordem crescente é: $menor_valor $maior_valor";
     }else{
-
+      $menor_valor = $valor1;
+      $maior_valor = $valor2;
+      echo "Os valores em ordem crescente é: $menor_valor $maior_valor";
     }
-
     
-
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
