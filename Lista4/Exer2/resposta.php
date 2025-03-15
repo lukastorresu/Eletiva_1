1318c000df59f86213dc6bdf535c5f7ba9fc1ed9
Lucas Torres <lukastorresu@gmail.com>
2025-03-15 00:08:34 -0300

    Função Maiusculo

--- Lista4/Exer2/resposta.php
+++ Lista4/Exer2/resposta.php
@@ -12,11 +12,11 @@
   <h1>Resposta</h1>
   <p>
     <?php
-    function contar($palavra){
-      echo "Quantidade de caracteres: ".strlen($palavra);
+    function Maiusculo($palavra){
+      echo "Palavra em maíusculo: ".strtoupper($palavra);
     }
     $palavra = strval($_POST['palavra']); 
-    contar($palavra);
+    Maiusculo($palavra);
     
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
