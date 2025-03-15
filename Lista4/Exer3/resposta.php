cc035b283b2a3ea9a90a6b71747464898b8bca4a
Lucas Torres <lukastorresu@gmail.com>
2025-03-15 00:16:27 -0300

    estrutura inicial da função

--- Lista4/Exer3/resposta.php
+++ Lista4/Exer3/resposta.php
@@ -12,15 +12,12 @@
   <h1>Resposta</h1>
   <p>
     <?php
-    function Maiusculo($palavra){
-      echo "Palavra em maíusculo: ".strtoupper($palavra)."<br>";
+    function Verificar($palavra1,$palavra2){
+
     }
-    function Minusculo($palavra){
-      echo "Palavra em minusculo: ".strtolower($palavra)."<br>";
-    }
-    $palavra = strval($_POST['palavra']); 
-    Maiusculo($palavra);
-    Minusculo($palavra);
+    $palavra1 = strval($_POST['palavra1']); 
+    $palavra2 = strval($_POST['palavra2']); 
+
    
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
