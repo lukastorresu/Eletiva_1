8a04238a39e2419eb3c7f312141ffa938a3f2e3a
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 18:11:16 -0300

    teste formatação

--- Lista4/Exer7/resposta.php
+++ Lista4/Exer7/resposta.php
@@ -12,15 +12,17 @@
   <h1>Resposta</h1>
   <p>
     <?php
-    function calcDiferencaDatas($data1, $data2) {
-      
+    function calcDiferencaDatas($data1, $data2){
+      $data1 = DateTime::createFromFormat('dd/mm/YYYY', $data1);
+      $data2 = DateTime::createFromFormat('dd/mm/YYYY', $data2);
+
     }
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $data1 = $_POST['data1'];
       $data2 = $_POST['data2'];
       $diferenca = calcDiferencaDatas($data1, $data2);
-      echo 
+      echo 'A diferença em dias é' . $diferenca . ".";
     }
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
