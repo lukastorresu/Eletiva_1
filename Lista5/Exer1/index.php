47e9e0ab68b4acec40db795294bbc0dadce32bd3
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 18:54:55 -0300

    corrigido título e adicionado estrutura FOR
    repeticao FOR para cadastrar diversos contatos

--- Lista5/Exer1/index.php
+++ Lista5/Exer1/index.php
@@ -13,21 +13,22 @@ declare(strict_types=1);
 </head>
 
 <body>
-  <h1>Exercício 1 - Ler string e retornar quantidade de caracteres</h1>
+  <h1>Exercício 1 - Cadastrar 5 contatos.</h1>
   <p>
 
   <form method="post" action="resposta.php">
-    <div class="row">
-      <div class="col-2">
-        <label for="palavra" class="form-label">Digite uma palavra:</label>
-        <input type="text" id="palavra" name="palavra" class="form-control" required="">
-      </div>
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+            <fieldset>
+                <legend>Contato <?php echo $i; ?></legend>
+                <label for="nome<?php echo $i; ?>">Nome:</label>
+                <input type="text" name="contatos[<?php echo $i; ?>][nome]" id="nome<?php echo $i; ?>" required="">
+            </fieldset>
+        <?php endfor; ?>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
 
-    </div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
-  </form>
-
-  </p>
+  </p>  
   </h1>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
