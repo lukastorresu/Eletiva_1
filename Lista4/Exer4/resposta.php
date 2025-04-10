619494350548e9dfb5a64797c7d31681c8ab4d58
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 15:48:24 -0300

    apresentar data se for válida

--- Lista4/Exer4/resposta.php
+++ Lista4/Exer4/resposta.php
@@ -20,7 +20,12 @@
     $dia = $_POST['dia'] ?? 0;
     $mes = $_POST['mes'] ?? 0;
     $ano = $_POST['ano'] ?? 0;
-
+    
+    if (validarData($dia, $mes, $ano)) {
+      echo sprintf("%02d/%02d/%04d", $dia, $mes, $ano);
+    } else {
+      echo "Data inválida";
+    }
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
