d52ca5bff6f6858f59db06c67586b6ec39c189c5
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 18:13:45 -0300

    estrutura if caso não haja diferença

--- Lista4/Exer7/resposta.php
+++ Lista4/Exer7/resposta.php
@@ -12,18 +12,21 @@
   <h1>Resposta</h1>
   <p>
     <?php
-    function calcDiferencaDatas($data1, $data2){
+    function calcDiferencaDatas($data1, $data2)
+    {
       $data1 = DateTime::createFromFormat('dd/mm/YYYY', $data1);
       $data2 = DateTime::createFromFormat('dd/mm/YYYY', $data2);
 
+      if ($data1 && $data2) {
+        return $data1->diff($data2)->days;
+      }
+      return false;
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      $data1 = $_POST['data1'];
-      $data2 = $_POST['data2'];
-      $diferenca = calcDiferencaDatas($data1, $data2);
-      echo 'A diferença em dias é' . $diferenca . ".";
-    }
+    $data1 = $_POST['data1'] ?? '';
+    $data2 = $_POST['data2'] ?? '';
+
+    echo "Diferença em dias: " . calcDiferencaDatas($data1, $data2) . ".";
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
