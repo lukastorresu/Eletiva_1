f023b30a004c9f0b937bca4900366f06c76c9a37
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 19:14:34 -0300

    reestruturado com arrays diferentes
    arrays diferentes para verificar se há duplicidade

--- Lista5/Exer1/resposta.php
+++ Lista5/Exer1/resposta.php
@@ -12,15 +12,11 @@
   <h1>Resposta</h1>
   <p>
     <?php
-    function verificarContatos(array $contatos): array {
-      $mapaContatos = [];
-      $telefonesCadastrados = [];
-      $duplicadosEncontrados = false;
-    }
-
     $contatos = $_POST['contatos'] ?? [];
-    $listaOrdenada = verificarContatos($contatos);
-    
+    $listaOrdenada = array();
+    $nomesCadastrados = array();
+    $telefonesCadastrados = array();
+
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
