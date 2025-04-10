ccf75cfdb7abfc7a2e5af34b3c4cf7220814e12a
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 19:48:47 -0300

    Formulário Inicial
    copiado do exercício 1

---
+++ Lista5/Exer2/index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Exercício 1 - Cadastrar 5 contatos.</h1>
+  <p>
+
+  <form method="post" action="resposta.php">
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+            <fieldset>
+                <legend>Contato <?php echo $i; ?></legend>
+                <label for="nome<?php echo $i; ?>">Nome:</label>
+                <input type="text" name="contatos[<?php echo $i; ?>][nome]" id="nome<?php echo $i; ?>" required="">
+                
+                <label for="telefone<?php echo $i; ?>">Telefone:</label>
+                <input type="text" name="contatos[<?php echo $i; ?>][telefone]" id="telefone<?php echo $i; ?>" required="">
+            </fieldset>
+        <?php endfor; ?>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+
+  </p>  
+  </h1>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Lista5/Exer2/resposta.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Resposta</h1>
+  <p>
+    <?php
+    $contatos = $_POST['contatos'] ?? [];
+    $listaOrdenada = array();
+    $nomesCadastrados = array();
+    $telefonesCadastrados = array();
+
+    foreach ($contatos as $contato) {
+      $nome = $contato['nome'];
+      $telefone = $contato['telefone'];
+
+      if (!in_array($nome, $nomesCadastrados) && !in_array($telefone, $telefonesCadastrados)) {
+        $mapaContatos[$nome] = $telefone;
+        $nomesCadastrados[] = $nome;
+        $telefonesCadastrados[] = $telefone;
+      } else {
+        echo "<p style='color: red;'>Aviso: Contato duplicado ($nome - $telefone) .</p>";
+      }
+    }
+
+    ksort($mapaContatos);
+    echo "<ul>";
+    foreach ($mapaContatos as $nome => $telefone) {
+      echo "<li><strong>$nome:</strong> $telefone</li>";
+    }
+    echo "</ul>";
+
+    ?></p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
