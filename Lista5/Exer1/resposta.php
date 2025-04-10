9273ad6f66b395dfba2f604a88212d48914b6096
Lucas Torres <lukastorresu@gmail.com>
2025-04-10 19:21:46 -0300

    exibir lista

--- Lista5/Exer1/resposta.php
+++ Lista5/Exer1/resposta.php
@@ -29,6 +29,11 @@
         echo "<p style='color: red;'>Aviso: Contato duplicado ($nome - $telefone) .</p>";
       }
     }
+    echo "<ul>";
+    foreach ($mapaContatos as $nome => $telefone) {
+      echo "<li><strong>$nome:</strong> $telefone</li>";
+    }
+    echo "</ul>";
 
     ?></p>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
